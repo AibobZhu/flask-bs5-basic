f77b161b91195202ebef08908e61df8555960665
Haibo Zhu <aibob.zhu@hotmail.com>
2021-11-13 20:44:15 +0800

    add test.js

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template, request, flash, Markup, Blueprint, current_app
+from flask import Flask, render_template, request, flash, Markup, Blueprint, current_app, url_for
 import warnings
 
 try:  # pragma: no cover
